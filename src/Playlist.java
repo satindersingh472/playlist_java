2da56c27da9670f261493e8086420a2281dfd9cc
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-12-28 20:22:13 -0700

    working_with_arraylist

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
 public class Playlist {
+    public static void main(String[] args){
+        ArrayList<String> desertIslandPlaylist = new ArrayList<>();
+        desertIslandPlaylist.add("signed to god");
+        desertIslandPlaylist.add("IDGAF");
+        desertIslandPlaylist.add("famous");
+        desertIslandPlaylist.add("racks and rounds");
+        desertIslandPlaylist.add("snitches get stiches");
+        desertIslandPlaylist.add("panjab");
+        desertIslandPlaylist.remove(1);
+        System.out.println(desertIslandPlaylist);
+        System.out.println(desertIslandPlaylist.size());
+        desertIslandPlaylist.set(0,"IDGAF");
+        desertIslandPlaylist.set(1,"signed to god");
+        System.out.println(desertIslandPlaylist);
+    }
 }
